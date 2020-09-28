b12409a2f2a509e1bc40fdc7deb4b262735828f1
KSCorto <ksc0029@mix.wvu.edu>
2020-09-27 20:53:43 -0400

    Lab 4

---
+++ About.php
@@ -0,0 +1,68 @@
+<?php
+require 'includes/header.php'?>
+<main>
+<link rel="stylesheet" href="css\About.css">
+        <!--
+            Class about
+
+            Includes Header of the page name and a sub header being what you'll find on the page
+         -->
+        <div class="about">
+            <h1>About Us</h1>
+            <p>
+                Meet the team that ensures that your content is quality.    
+                From culinary appeal to supernatural elements, our team covers all bases to ensure that your viewing experience is immaculate.
+            </p>
+        </div>
+        <div class="row">
+            <div class="column">
+                <!-- 
+                    Class card
+                    Creates a card with company employees' contact information
+                 -->
+                <div class="card">
+                    <img src="Images/Tabitha Brown.jpg">
+                    <div class="container">
+                        <h2>Food Expert</h2>
+                        <h3>Tabitha Brown</h3>
+                        <p><b>Phone:</b> 555-555-0001</p>
+                        <p><b>Email:</b> <button>iamtbrown@company.com</button></p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="Images/Awkwafina.jpg">
+                    <div class="container">
+                        <h2>Vibe Checker</h2>
+                        <h3>Nora Lum</h3>
+                        <p><b>Phone:</b> 555-555-0010</p>
+                        <p><b>Email:</b> <button>awkwafina@company.com</button></p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="Images/Ghosthoney.jpg">
+                    <div class="container">
+                        <h2>Supernatural Expert</h2>
+                        <h3>Tyler Gaca</h3>
+                        <p><b>Phone:</b> 555-555-0011</p>
+                        <p><b>Email:</b> <button>ghosthoney@company.com</button></p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="Images/John Mulaney.jpg">
+                    <div class="container">
+                        <h2>Comedy Expert</h2>
+                        <h3>John Mulaney</h3>
+                        <p><b>Phone:</b> 555-555-0100</p>
+                        <p><b>Email:</b> <button>newkid@company.com</button></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</main>
