b4c11d0565854c6d052c61c8857a32c5147ef993
KSCorto <ksc0029@mix.wvu.edu>
2020-10-11 22:57:36 -0400

    Lab 6

--- includes/gallery-helper.php
+++ includes/gallery-helper.php
@@ -1,28 +1,24 @@
-<?php
-session_start();
+<?php 
 
-define('KB', 1024);
+require 'dbhandler.php';
 define('MB', 1048576);
 
-if (isset($_POST['prof-submit'])) {
-    require 'dbhandler.php';
-   
-    $uname = $_SESSION['username'];
-
-    $file = $_FILES['prof-image'];
+if(isset($_POST['gallery-submit'])) {
+    $file = $_FILES['gallery-image'];
     $file_name = $file['name'];
     $file_temp_name = $file['tmp_name'];
     $file_error = $file['error'];
     $file_size = $file['size'];
 
-    $bio = $POST['bio'];
+    $title = $_POST['title'];
+    $descript = $_POST['descript'];
 
     $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
     $allowed = array('jpg', 'jpeg', 'png', 'svg');
 
     if ($file_error !== 0) {
-        header("Location: ../profile.php?error=UploadError");
+        header("Location: ../admin.php?error=UploadError");
         exit();
     }
     if (!in_array($ext, $allowed)){
@@ -30,29 +26,30 @@ if (isset($_POST['prof-submit'])) {
         exit();
     }
     if ($file_size > 4*MB) {
-        header("Location: ../profile.php?error=FileSizeExceeded");
+        header("Location: ../admin.php?error=FileSizeExceeded");
         exit();
     }
     else {
         $new_name = uniqid('', true).".".$ext;
-        $destination = '../uploads/'.$new_name;
+        $destination = '../reviews/'.$new_name;
 
-        $sql = "UPDATE profile SET picpath=?, bio=? WHERE uname=?";
+        $sql = "INSERT INTO reviews (title, descript, picpath) VALUES (?, ?, ?)";
         $stmt = mysqli_stmt_init($conn);
 
         if(!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../profile.php?error=SDLInjection");
+            header("Location: ../login.php?error=SDLInjection");
             exit();
         }
         else {
-            mysqli_stmt_bind_param($stmt, "sss", $destination, $bio, $uname);
+            mysqli_stmt_bind_param($stmt, "sss", $title, $descript, $new_name);
             mysqli_stmt_execute($stmt);
             mysqli_stmt_store_result($stmt);
 
             move_uploaded_file($file_temp_name, $destination);
-            header("Location: ../profile.php?error=UploadSuccess");
+            header("Location: ../admin.php?error=UploadSuccess");
             exit();
         }
 
     }
 }
+
